67a9d98aeda9a9a014d59c22df2a04dfc05606c0
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-04 21:53:57 +0500

    created the initial layout of the app

---
+++ src/components/TransactionList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const TransactionList = () => {
+	return (
+		<>
+			<h3>History</h3>
+			<ul className="list">
+				<li className="minus">
+					Cash <span>-$400</span>
+					<button className="delete-btn">
+						<i className="las la-minus-circle"></i>
+					</button>
+				</li>
+			</ul>
+		</>
+	);
+};
