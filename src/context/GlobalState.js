559d933a96afa045e32a41b7f9739164a2c8bd93
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-05 17:30:00 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -40,8 +40,15 @@ export const GlobalContext = createContext(intitalState);
 export const GlobalProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(AppReducer, intitalState);
 
+	// Actions
+	const deleteTransaction = (id) => {
+		dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+	};
+
 	return (
-		<GlobalContext.Provider value={{ transactions: state.transactions }}>
+		<GlobalContext.Provider
+			value={{ transactions: state.transactions, deleteTransaction }}
+		>
 			{children}
 		</GlobalContext.Provider>
 	);
